a24476dcb5ed56e4689cfee1d43df13fb9de8b72
Ciemny Roupiq <k27radek@gmail.com>
2023-02-25 16:56:14 +0100

    fixed movement bug

--- engine/scripts/Entity.gd
+++ engine/scripts/Entity.gd
@@ -6,6 +6,9 @@ export var floor_friction := 0.75
 export var gravity := Vector2(0.0, 50.0)
 export var keep_speed_after_rotation = false
 export var rotation_speed := 1.0
+var isOnFloor := false
+var isOnRoof := false
+var isOnWall := false
 var rotated := 0.0
 
 var falling_time := 0.0 
@@ -27,19 +30,69 @@ func default_phisics(delta: float, acceleration: Vector2 = Vector2(0, 0)) -> voi
 	acceleration += gravity
 	_velocity += acceleration
 	_velocity *= friction
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL, true, 4, 0.785398, false)
+	if abs(_velocity.x) < 0.1:
+		_velocity.x = 0;
+	if abs(_velocity.y) < 0.1:
+		_velocity.y = 0;
+	#print(_velocity)
+	_velocity = move(_velocity)
+	#print(_velocity)
+	print(isOnFloor)
+	#print(delta)
+
 	
+func move(velocity : Vector2, steps : int = 0) -> Vector2:
+	var colision1 = move_and_collide(Vector2.ZERO, true, true, true)
+	if colision1:
+		print("jup")
+
+	for i in range(abs(velocity.y)):
+		if velocity.y > 0:
+			var colision = move_and_collide(Vector2.DOWN, true, true, true)
+			if colision:
+				isOnFloor = true
+				velocity.y = 0
+				break
+			else:
+				isOnFloor = false
+		elif velocity.y < 0:
+			var colision = move_and_collide(Vector2.UP, true, true, true)
+			isOnFloor = false
+			if colision:
+				velocity.y = 0
+				break
+		
+		position.y += sign(velocity.y)  * get_physics_process_delta_time()
+	
+	for i in range(abs(velocity.x)):
+		if velocity.x > 0:
+			var colision = move_and_collide(Vector2.RIGHT, true, true, true)
+			if colision:
+				velocity.x = 0
+				break
+		elif velocity.x < 0:
+			var colision = move_and_collide(Vector2.LEFT, true, true, true)
+			if colision:
+				velocity.x = 0
+				break
+
+		position.x += sign(velocity.x) * get_physics_process_delta_time()
+	
+	return velocity
+
+
 func calculate_when_character_was_on_floor():
-	if is_on_floor():
+	if isOnFloor:
 		on_floor = 0
 		falling_time = 0.0
 	else:
 		on_floor += get_physics_process_delta_time()
 
 func change_gravity(rotations: int, keep_speed: bool):
-	position = position.rotated(deg2rad(90))
-	_velocity = Vector2.ZERO
-	rotation_degrees += 90
+	for i in range(rotations):
+		position = position.rotated(deg2rad(90))
+		_velocity = Vector2.ZERO
+		rotation_degrees += 90
 
 func rotate_90_degrees_CCW(vec : Vector2) -> Vector2:
 	vec = Vector2(vec.y, -vec.x)
