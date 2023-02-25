0530edae251b58bdc3640b241e6a3c17afaddc43
Ciemny Roupiq <k27radek@gmail.com>
2023-02-25 14:30:23 +0100

    Animations!

--- engine/scripts/Player.gd
+++ engine/scripts/Player.gd
@@ -5,16 +5,18 @@ export var walk_speed = 120
 export var jump_speed = 500
 export var coyote_time = 0.1
 export var jump_time = 0.1
+onready var _animated_sprite = $AnimatedSprite
 
 func _physics_process(delta: float) -> void:
 	calculate_when_character_was_on_floor()
 	var	direction := get_direction()
 	var acceleration := calculate_move_acceleration(direction, Vector2(walk_speed, jump_speed))
-	acceleration
+	change_sprite_rotation(direction)
 	default_phisics(delta, acceleration)
 
 func _process(delta: float) -> void:
 	manage_changing_gravity()
+	animations()
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -34,7 +36,6 @@ func calculate_move_acceleration(
 		direction: Vector2,
 		speed: Vector2
 	) -> Vector2:
-		
 	var new_velocity := direction * speed
 	return new_velocity
 
@@ -46,8 +47,25 @@ func manage_changing_gravity():
 		rotations += 2
 	if(Input.is_action_just_pressed("gravity_left")):
 		rotations += 3
-	change_gravity(rotations%4, keep_speed_after_rotation)
-	emit_signal("rotate_gravity", rotations)
+	if rotations != 0:
+		emit_signal("rotate_gravity", rotations)
+		change_gravity(rotations%4, keep_speed_after_rotation)
 
-	
-	
+func animations():
+	if not on_floor < coyote_time:
+		if _velocity.y >= 0:
+			_animated_sprite.play("fallingDown")
+		else:
+			_animated_sprite.play("fallingUp")
+	elif Input.is_action_pressed("move_right"):
+		_animated_sprite.play("walking")
+	elif Input.is_action_pressed("move_left"):
+		_animated_sprite.play("walking")
+	else:
+		_animated_sprite.play("default")
+
+func change_sprite_rotation(direction: Vector2):
+	if direction.x > 0:
+		_animated_sprite.flip_h = false
+	if direction.x < 0:
+		_animated_sprite.flip_h = true
---
+++ engine/scripts/meta.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+export var keep_speed_after_rotation = false
+var screen_size : Vector2
+var center_of_screen : Vector2
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	center_of_screen = screen_size / 2
+	var Player = get_parent().get_node("Player")
+	Player.connect("rotate_gravity", self, "_on_Player_rotate_gravity")
+
+func _on_Player_rotate_gravity(rotations_to_perform) -> void:
+	change_gravity(rotations_to_perform, keep_speed_after_rotation)
+
+func rotate_90_degrees_CCW(vec : Vector2) -> Vector2:
+	vec = Vector2(vec.y, -vec.x)
+	return vec
+
+func change_gravity(rotations: int, keep_speed: bool):
+	for i in range(rotations):
+		position -= center_of_screen
+		position = rotate_90_degrees_CCW(position)
+		position += center_of_screen
+		rotation_degrees -= 90
