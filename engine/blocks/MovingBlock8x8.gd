b7f68b565af9366b1081bcc4e85aa043f2b8d952
wojtekmal <wojciech.malinowski.100@gmail.com>
2023-03-10 21:40:59 +0100

    Falling without rotations is working.

--- engine/blocks/MovingBlock8x8.gd
+++ engine/blocks/MovingBlock8x8.gd
@@ -4,24 +4,32 @@ class_name MovingBlock8x8 extends CharacterBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-@export var board_cords: Vector2 : set = update_board_cords
-@export var board_dimensions: Vector2 : set = update_board_dimensions
-
+@export var board_cords: Vector2i : set = set_board_cords
+@export var board_dimensions: Vector2i : set = set_board_dimensions
+@export var is_falling : bool : set = set_is_falling
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func update_board_dimensions(newValue):
+func set_board_dimensions(newValue):
 	board_dimensions = newValue
-	update_board_cords(board_cords)
+	set_board_cords(board_cords)
 
-func update_board_cords(newValue):
+func set_board_cords(newValue):
 	board_cords = newValue
-	set_position(Vector2(-(board_dimensions.x / 2 - board_cords.x) * 64 + 32, -(board_dimensions.y / 2 - board_cords.y) * 64 + 32))
+	#print_debug(board_dimensions)
+	#print_debug(board_cords)
+	#print_debug("\n")
+	set_position(Vector2i(board_cords.x * 64 + 32 - board_dimensions.x * 32, board_cords.y * 64 + 32 - board_dimensions.y * 32))
 
-func get_class():
+#func get_class():
+#	return "MovingBlock8x8"
+
+func get_real_class():
 	return "MovingBlock8x8"
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
+
+func set_is_falling(new_value):
+	is_falling = new_value
