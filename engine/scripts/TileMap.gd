dd1d29b514d453656d51954fd2a039c80f30134b
wojtekmal <wojciech.malinowski.100@gmail.com>
2023-03-20 12:53:15 +0100

    Everything should be ready for level creation.

--- engine/scripts/TileMap.gd
+++ engine/scripts/TileMap.gd
@@ -1,7 +1,7 @@
 @tool
 class_name MyTileMap
 extends TileMap
-
+@export var tilemap_dimensions : Vector2i : set = set_tilemap_dimensions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +14,22 @@ func _process(delta):
 
 func get_real_class():
 	return "TileMap"
+
+func set_tilemap_dimensions(newValue):
+	for i in range(-tilemap_dimensions.x * 4 - 1, tilemap_dimensions.x * 4 + 1):
+		set_cell(0, Vector2i(i, tilemap_dimensions.y * 4), -1, Vector2i(0, 0))
+		set_cell(0, Vector2i(i, -tilemap_dimensions.y * 4 - 1), -1, Vector2i(0, 0))
+	
+	for i in range(-tilemap_dimensions.y * 4 - 1, tilemap_dimensions.y * 4 + 1):
+		set_cell(0, Vector2i(-tilemap_dimensions.x * 4 - 1, i), -1, Vector2i(0, 0))
+		set_cell(0, Vector2i(tilemap_dimensions.x * 4, i), -1, Vector2i(0, 0))
+		
+	for i in range(-newValue.x * 4 - 1, newValue.x * 4 + 1):
+		set_cell(0, Vector2i(i, newValue.y * 4), 0, Vector2i(0, 0))
+		set_cell(0, Vector2i(i, -newValue.y * 4 - 1), 0, Vector2i(0, 0))
+
+	for i in range(-newValue.y * 4 - 1, newValue.y * 4 + 1):
+		set_cell(0, Vector2i(-newValue.x * 4 - 1, i), 0, Vector2i(0, 0))
+		set_cell(0, Vector2i(newValue.x * 4, i), 0, Vector2i(0, 0))
+	
+	tilemap_dimensions = newValue
