bc95e5058cc0f7e3bf739fb5e73b8d0559a36ce6
wojtekmal <wojciech.malinowski.100@gmail.com>
2023-03-26 13:09:40 +0200

    Added a finish block.

--- engine/scripts/finish_block.gd
+++ engine/scripts/finish_block.gd
@@ -1,5 +1,5 @@
 @tool
-class_name StaticBlock8x8 extends StaticBody2D
+class_name FinishBlock extends StaticBody2D
 
 @export var board_cords: Vector2i : set = set_board_cords
 @export var board_dimensions: Vector2i : set = set_board_dimensions
