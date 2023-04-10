55a311706fc32027cf72df878cefebdc019d5cd6
roupiq <k27radek@gmail.com>
2023-04-10 16:27:03 +0200

    stopwatch + pause

--- engine/scripts/global.gd
+++ engine/scripts/global.gd
@@ -1,3 +1,19 @@
 extends Node
 
+<<<<<<< HEAD
 var current_level : String = "1"
+=======
+var current_level
+
+# This is a dictionary holding all levels by their numbers.
+# Here all important information about levels is held in this format:
+#
+#"level_number": {
+# 	"unlocked": bool,
+#	"finished_parts": int, # This is 0 or 1 depending on whether the level was finished.
+#	"rotation_parts": int, # This is 0, 1, or 2.
+#	"time_parts": int, # This is 0, 1 or 2.
+#}
+
+var levels := {}
+>>>>>>> 6b86b444846cd737c84e8093863f41d939f28cb0
