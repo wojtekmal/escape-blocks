8f79355d1948949b478ae0e5c4595c56508d7180
roupiq <k27radek@gmail.com>
2023-04-11 19:20:25 +0200

    saving

--- engine/scripts/global.gd
+++ engine/scripts/global.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var current_level : String = "1"
-
+ 
 # This is a dictionary holding all levels by their numbers.
 # Here all important information about levels is held in this format:
 #
@@ -12,4 +12,33 @@ var current_level : String = "1"
 #	"time_parts": int, # This is 0, 1 or 2.
 #}
 
+func _ready():
+	name = "global"
+	load_data()
+
 var levels := {}
+
+var saved_var := {
+	"levels" : levels,
+	"current_level" : current_level,
+}
+
+func load_data():
+	var file = FileAccess.open(
+		"user://" + name +".dat", 
+		FileAccess.READ
+	)
+	if file == null:
+		save()
+		return
+	var content = file.get_var()
+	for variable in saved_var.keys():
+		saved_var[variable] = content[variable]
+
+func save(content = saved_var):
+	var file = FileAccess.open(
+		"user://" + name + ".dat", 
+		FileAccess.WRITE,
+	)
+	file.store_var(content)
+	print("saved " + name)
