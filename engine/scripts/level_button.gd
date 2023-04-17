224638e800e5a134f24cf03c59b19a1cf6196107
roupiq <k27radek@gmail.com>
2023-04-17 18:48:42 +0200

    added very important NULL level

--- engine/scripts/level_button.gd
+++ engine/scripts/level_button.gd
@@ -4,19 +4,16 @@ extends Node2D
 
 signal button_pressed
 
-@export var label_text : String : set = set_label_text
-#@export var dependencies : Array[String] = []
-
 @onready var pressable_button := $TextureButton
 #var is_unlocked : bool = false
-
-
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var label = $TextureButton/Label
 	#print(label_text)
 	
-	label.text = label_text
+	label.text = name
+	set_label_text(name)
 	pressable_button.pressed.connect(on_button_pressed)
 
 
@@ -27,9 +24,15 @@ func _process(delta):
 func set_label_text(new_value):
 	#print("check")
 	var label = $TextureButton/Label
-	label.text = new_value
+	if(global.levels.has(name)):
+		label.text = new_value
+	else:
+		label.text = "NULL"
+		name = "NULL"
 	#print(label.text)
-	label_text = new_value
 
 func on_button_pressed():
-	emit_signal("button_pressed", label_text)
+	emit_signal("button_pressed", name)
+
+func _on_renamed():
+	set_label_text(name)
