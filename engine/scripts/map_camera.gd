715ec0edcc451159ca4f5663cd941aaa8a31c7a1
wojtekmal <wojciech.malinowski.100@gmail.com>
2023-05-26 08:28:15 +0200

    Forgot to commit these files.

--- engine/scripts/map_camera.gd
+++ engine/scripts/map_camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 var _previousPosition: Vector2 = Vector2(0, 0);
 var _moveCamera: bool = false;
+var zoom_factor = 1.0
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
@@ -13,5 +14,9 @@ func _unhandled_input(event: InputEvent):
 			_moveCamera = false;
 	elif event is InputEventMouseMotion && _moveCamera:
 		get_viewport().set_input_as_handled();
-		position += (_previousPosition - event.position);
+		position += (_previousPosition - event.position) / zoom_factor;
 		_previousPosition = event.position;
+		position.x = max(position.x, -800)
+		position.x = min(position.x, 1500)
+		position.y = max(position.y, -800)
+		position.y = min(position.y, 1000)
