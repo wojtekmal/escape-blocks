65c817ce6a66054c147c4a0b62ff9c59077ffbc3
wojtekmal <wojciech.malinowski.100@gmail.com>
2023-08-14 10:42:08 +0200

    Added the whole folder this time.

---
+++ tas/wojtekmal_parkour.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sleep 3
+xdotool key R
+xdotool keydown W
+sleep 0.4
+xdotool keydown A
+sleep 0.3
+xdotool keyup W
+sleep 0.5
+xdotool keydown Left
+sleep 0.1
+xdotool keyup Left
+sleep 0.3
+xdotool keydown W
+sleep 0.5
+xdotool keyup W
+xdotool keyup A
+sleep 0.5
